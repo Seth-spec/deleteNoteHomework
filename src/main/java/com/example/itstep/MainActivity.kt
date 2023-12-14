c029e995d713e21a7ababae4c221a4d04d185bd2
Gvaroo <faiababe@gmail.com>
2023-12-14 12:48:03 +0400

    delete note homework

---
+++ src/main/java/com/example/itstep/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.example.itstep
+
+import android.app.Dialog
+import android.content.Intent
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.itstep.databinding.ActivityMainBinding
+import com.example.itstep.databinding.DialogLayoutBinding
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val adapter = UsersRV()
+    private lateinit var noteDataBase: AppDataBase
+    private lateinit var noteDao: NoteDao
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        noteDataBase = AppDataBase.getInstance(this)
+        noteDao = noteDataBase.noteDao()
+
+        setContentView(binding.root)
+
+        binding.apply {
+            recyclerView.adapter = adapter
+            recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+        }
+
+        binding.button.setOnClickListener {
+            showDialog()
+        }
+
+        lifecycleScope.launch {
+            noteDao.getNotes().collect { notes ->
+                adapter.updateList(notes)
+            }
+        }
+
+        adapter.itemCallback = { note ->
+//            startActivity(Intent(this, SecondActivity::class.java).apply {
+//                putExtra("name", name)
+//            })
+            lifecycleScope.launch {
+                noteDao.deleteNote(note)
+            }
+        }
+    }
+
+    private fun showDialog() {
+        val dialogBinding = DialogLayoutBinding.inflate(
+            LayoutInflater.from(this)
+        )
+        val dialog = Dialog(this)
+
+        val displayMetrics = DisplayMetrics()
+        val with = displayMetrics.widthPixels * 0.8
+
+        dialog.setContentView(
+            dialogBinding.root
+        )
+
+
+        dialogBinding.saveBtn.setOnClickListener {
+            lifecycleScope.launch {
+                noteDao.insertNote(
+                    Note(
+                        title = dialogBinding.titleET.text.toString(),
+                        note = dialogBinding.noteET.text.toString()
+                    )
+                )
+                dialog.dismiss()
+            }
+        }
+        dialog.show()
+    }
+}
---
+++ src/main/java/com/example/itstep/UsersRV.kt
@@ -0,0 +1,56 @@
+package com.example.itstep
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.itstep.databinding.SingleItemBinding
+
+class UsersRV(): RecyclerView.Adapter<UsersRV.UserViewHolder>() {
+
+    var itemCallback: ((Note) -> Unit)? = null
+
+    private var noteList = emptyList<Note>()
+
+    inner class UserViewHolder(
+        private val binding: SingleItemBinding
+    ): RecyclerView.ViewHolder(binding.root) {
+        fun bind() {
+            val item = getItem(adapterPosition)
+
+            binding.apply {
+                titleTV.text = item.title
+                noteTV.text = item.note
+
+                root.setOnClickListener {
+                    itemCallback?.invoke(item)
+                }
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        return UserViewHolder(
+            SingleItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int = noteList.size
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        holder.bind()
+    }
+
+    private fun getItem(position: Int): Note {
+        return noteList[position]
+    }
+
+    fun updateList(newList: List<Note>) {
+        noteList = newList
+        notifyDataSetChanged()
+    }
+}
